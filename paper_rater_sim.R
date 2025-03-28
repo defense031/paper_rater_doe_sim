dc218b8819badaee89371fea5ddfad96d69a70d2
Austin S <RUNFAST031@GMAIL.COM>
2025-03-28 14:45:39 -0400

    first commit

---
+++ paper_rater_sim.R
@@ -0,0 +1,8 @@
+# This script will create a simulation that applies a DOE concept to test the final screening under multiple 
+## paper to rater matching methods
+
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+
+print("Hello, Rachel!")
